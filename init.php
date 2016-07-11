ed8694bf60711885a2fe8fe9ff65fbda0e400bf7
Alexey Berdnikov <aberdnikov@gmail.com>
2016-07-11 20:57:08 +0500

    fixed

---
+++ init.php
@@ -0,0 +1,5 @@
+<?php
+\Larakit\StaticFiles\Manager::package('larakit/sf-fancybox')
+    ->setSourceDir('public')
+    ->cssPackage('jquery.fancybox.css')
+    ->jsPackage('jquery.fancybox.pack.js');
